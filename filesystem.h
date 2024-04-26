f0a28c84d41d8c06840f522c1e1a9821b33cea25
Rachel Nguyen <118865400+rachel0608@users.noreply.github.com>
2024-04-26 14:13:11 -0400

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -6,59 +6,37 @@
 #include "fat.h"
 
 typedef struct {
-    uint16_t bytes_per_sector;      // Bytes per sector (512)
-    uint8_t sectors_per_cluster;    // Sectors per cluster (powers of 2 from 1-128)
-    uint16_t total_sectors;         // Total number of sectors 
-    uint16_t sectors_per_fat;       // Sectors per FAT
-    uint16_t sectors_per_track;     // Sectors per track
-    uint16_t num_heads;             // Number of heads
-    uint32_t hidden_sectors;        // Number of hidden sectors before this partition
-    uint32_t total_sectors_big;     // Total number of sectors (if total_sectors is 0)
-} BootSector;
+    uint8_t file_size_mb;       // file size in MB (default is 1MB)
+    uint8_t FAT_offset;         // offset of first FAT block location
+    uint8_t FREEMAP_offset;      // offset of first Free-Bitmap location
+    uint8_t ROOTDIR_offset;     // offset of first ROOTDIR block location
+    uint8_t DATA_offset;        // offset of first DATABLOCK location
+    uint16_t block_size;        // block size will be standard 512 bytes
+    uint32_t file_size_blocks;  // file size in blocks (1MB = 2048 blocks)
+} superblock;
 
 typedef struct {
-  char* abs_path; // Absolute path
-  int file_desc; // File descriptor
-  char* access; // File access type
-  int position; // File position
-  int group_id;
-  int owner_id;
+	char* abs_path; // Absolute path
+	int file_desc; // File descriptor
+	char* access; // File access type
+	int position; // File position
 } FileHandle; // FileHandle loosely based on Unix systems’s FileHandle (may not need?)
 
-typedef struct {
-    char* username;     // Username
-    char* password;     // Password
-    int user_id;        // User ID (super user = 0 or regular user)
-} User;
-
-typedef struct {
-    char* groupname;    // Group name
-    int group_id;       // Group ID
-    int* member_ids;    // Array of user IDs who are members of this group
-    int num_members;    // Number of members in the group
-} Group;
-
-// tEchNicaLLy filename = directory = path
-
-FileHandle* f_open(char* filename, char* access, User* user); // access = w/w+, r/r+, a/a+ maybe add group later as param
+FileHandle* f_open(char* path, char* access); // access = w/w+, r/r+, a/a+ maybe add group later as param
 int f_read(FileHandle file, void* buffer, size_t bytes);
 int f_write(FileHandle file, void* buffer, size_t bytes);
-void f_close(FileHandle file, User* user);
+void f_close(FileHandle file);
 int f_seek(FileHandle file, long offset, int whence);
 void f_rewind(FileHandle file);
 int f_stat(FileHandle file, struct stat *buffer);
-int f_remove(const char* filename);
-DirectoryEntry* f_opendir(char* directory);
-DirectoryEntry* f_readdir(char* directory);
+int f_remove(const char* path);
+DirectoryEntry* f_opendir(char* path);
+DirectoryEntry* f_readdir(char* path);
 int f_closedir(char* path);
 int f_mkdir(char* path);
 int f_rmdir(char* path);
 
 // f_mount(); // Extra credit
 // f_umount(); // Extra credit
-
-// Pointers to heads of free lists
-DirectoryEntry* freeHeadDE = NULL;
-FatEntry* freeHeadFE = NULL;
 
 #endif
